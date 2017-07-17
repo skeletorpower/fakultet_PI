2ed481ef40b6a3ce6faf92312cd7355d90c4848f
Samsung <Samsung@DESKTOP-0SD882A>
2017-07-17 18:19:59 +0200

    dodata dugmad za osnovnu funkcionalnost otvaranja drugih elemanata guia

--- Banka/src/gui/MainFrame.java
+++ Banka/src/gui/MainFrame.java
@@ -1,9 +1,108 @@
 package gui;
 
-public class MainFrame {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import database.DBConnection;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame{
+	private JButton btnUpravljanjeRacunima = new JButton();
+	
+	
+	//dodacu jos btn po zelji
+	
+	public MainFrame(){
+		DBConnection.getConnection();
+		setSize(566, 400);
+		setMinimumSize(new Dimension(500, 300));
+		setTitle("Banka");
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(new MigLayout("", "[][][][][][][]", "[][][][]"));
+		
+		JLabel lblNewLabel = new JLabel("Upravljanje ranucima");
+		getContentPane().add(lblNewLabel, "cell 0 0,alignx center,aligny center");
+		
+		JLabel lblNalogZaTransfer = new JLabel("Nalozi");
+		getContentPane().add(lblNalogZaTransfer, "cell 2 0,alignx center,aligny center");
+		
+		JLabel lblNewLabel_1 = new JLabel("Import stavki placanja");
+		getContentPane().add(lblNewLabel_1, "cell 4 0,alignx center,aligny center");
+		
+		JButton btnUpRacunima = new JButton("Upravljanje racunima");
+		getContentPane().add(btnUpRacunima, "cell 0 1,alignx center,aligny center");
+		
+		JButton btnNalogZaTransfer = new JButton("Nalog za transfer");
+		getContentPane().add(btnNalogZaTransfer, "cell 2 1,alignx center,aligny center");
+		
+		JButton btnNewButton = new JButton("Import stavki placanja");
+		getContentPane().add(btnNewButton, "cell 4 1,alignx center,aligny center");
+		
+		JButton btnNalogZaPrenos = new JButton("Nalog za prenos izvoda");
+		getContentPane().add(btnNalogZaPrenos, "cell 2 2,alignx center,aligny center");
+		
+		btnUpravljanjeRacunima.setText("Upravljanje racunima");
+		btnUpravljanjeRacunima.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				//DUGME TREBA DA OTVORI DIJALOG ZA UNOS I BRISANJE RACUNA
+			}
+		});
+		
+	
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public void setVisible(boolean b) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
